d8b4a95fe735f15dc2f10beb1117ba7656b73f70
systemNEO <hpb@digital-place.de>
2013-06-04 21:35:26 +0200

    Fixed issue
    Fixed issue: Prevented NullPointerException.

--- src/de/systemNEO/recipes/Recipes.java
+++ src/de/systemNEO/recipes/Recipes.java
@@ -253,12 +253,11 @@ public final class Recipes extends JavaPlugin implements Listener {
 		String[] stacksDefinition = newIndex.split(",");
 		ItemStack[] stacks = new ItemStack[10];
 		ItemStack[] originalStacks = getRecipeOriginal(currentGroupIndex);
-		int originalMaxStackSize =  originalStacks.length - 1;
-				
+		
 		for(int i = 0; i < 10; ++i) {
 			
 			// Sicherstellen, dass nicht auf etwas leeres zugegriffen wird.
-			if(i > originalMaxStackSize || originalStacks[i] == null) {
+			if(originalStacks == null || originalStacks[i] == null) {
 				
 				stacks[i] = new ItemStack(Material.AIR);
 				
